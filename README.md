52fead543bcac13ae482ae0d03cfee048b4810f7
Peter Hu <Peter.Hu1@huawei.com>
2023-03-15 09:45:30 +0000

    update file

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # rm_tool
-##
+## Pipeline
+ack > search_res.log
+
+src/main.c process the log (read and rm)
+
 remove specific lines using Linux system call.
 
 sed -i '4d' 'test/fops.h'
 sed -i '1,2d' 'test/fops.c'
 
 generate deletedLog with timestamp.
+
+## Choice：
+1. rm function
+2. rm variable/single line
--- src/main.c
+++ src/main.c
@@ -1,4 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+char filename[1000]; // search_res.log
+char filename_last[1000];
+void file_data(FILE *fp){
+    int lines = 0; // lines of the file
+    int ch = 0;
+    int len = 0;//  length of line
+    int max_len = 0;//  max length of line
+    int max_file_size =0; // file length size 
+    while((ch = fgetc(fp))!=EOF){//!
+        ++len;
+        if(ch == '\n'){
+            if(max_len < len)
+                max_len = len;
+            ++lines;
+            len = 0;
+        }
+    }
+    if(len)
+        ++lines;
+    printf("Lines of the file: %d \n",lines);
+    printf("Max_len: %d \n",max_len);
+    rewind(fp);
+}
 void print_deleteList(){
     FILE * fp;
     fp = fopen ("deleteList.sh", "w+");
@@ -7,7 +33,111 @@ void print_deleteList(){
     fprintf(fp, "sed -i '%d,%dd' '%s'\n", 1,2, "test/fops.c");
     fclose(fp);
 }
+
+void process_search_log(FILE *fp){
+    int lines = 0; // lines of the file
+    int ch = 0;
+    int loop_id=1;
+    int len = 0;//  length of line, changing from 0 - current value
+    int max_len = 0;//  max length of line
+    int max_file_size =0; // file length size 
+    bool first_colon=true;
+    bool second_colon=false;
+    bool first_hit_colon=true;
+    int first_colon_len=0;
+    bool same_file=false;
+    int operation;
+    printf("----------------rm id: %d----------------\n",loop_id++);
+    while((ch = fgetc(fp))!=EOF){//!
+        if(ch==':'){
+            if (first_colon){
+                if(!first_hit_colon){
+                    // printf("%d",strncmp(filename, filename_last, len)); //debug
+                   if(strncmp(filename, filename_last, len) == 0){
+                        same_file=true;
+                        // printf("%d,%d",first_hit_colon,same_file); //debug
+                    }
+                }
+                else{
+                    first_hit_colon=false;
+                }
+                strncpy(filename_last, filename, len);
+                
+                if (first_hit_colon||!same_file){
+                    printf("file name: |");
+                    for(int i=0;i<len;i++){
+                        printf("%c",filename[i]);
+                    }
+                    printf("| (range of file name: %d)\n ",len);
+                }
+                
+                first_colon_len=len;
+                first_colon_len++;
+                first_colon=false;
+                second_colon=true;
+                 
+            }
+            else if(second_colon){
+                printf("line: |");
+                for(int i=first_colon_len;i<len;i++){
+                    printf("%c",filename[i]);
+                }     
+                printf("| (range of len: %d-%d), \n \n",first_colon_len,len);
+                second_colon=false;
+                same_file=false;
+                while( 1 ){
+                    printf("Please enter your operation choice: 2 for [DELETE] and 1 for [NOT]. \n");    
+                    scanf("%d",&operation);
+                    if (operation == 1){
+                        printf("[NOT] Valid input, well received!\n");
+                        break;
+                    }    
+                    if (operation==2){
+                        printf("[DELETE] Valid input, well received!\n");
+                        break;
+                    }    
+                    else if(operation != 2 && operation!=1){
+                        printf("Failure, invalid input %d\n",operation);
+                        continue;
+                    }
+                }   
+                      
+               
+
+            }
+        }
+
+        filename[len]=ch;
+        ++len;
+        if(ch == '\n'){
+            if(max_len < len)
+                max_len = len;
+            ++lines;
+            len = 0;
+            first_colon=true;
+            second_colon=false;
+            printf("---rm id: %d---\n",loop_id++);
+        }
+    }
+    if(len)
+        ++lines;
+    printf("Lines of the file: %d \n",lines);
+    printf("Max_len: %d \n",max_len);
+    rewind(fp);
+}
+
 int main(){
-   print_deleteList();  
+    FILE *fp;
+    fp= fopen("./log/search_res.log","r");
+    printf("------WELCOME to USE rm_tool 2023!------\n");   
+    if(fp == NULL)
+    {
+      printf("Error when opening the file!\n");   
+      exit(1);             
+    }
+    file_data(fp);
+    process_search_log(fp);
+    fclose(fp);  
+    print_deleteList();
    return(0);
 }
